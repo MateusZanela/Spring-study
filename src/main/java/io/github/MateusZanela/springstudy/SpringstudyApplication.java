f573561e053258bcab27d3524cdee1a9f87b629c
Mateus Zanela <984895@lojasrenner.com.br>
2026-03-05 09:20:41 -0300

    Removing hello world

--- src/main/java/io/github/MateusZanela/springstudy/SpringstudyApplication.java
+++ src/main/java/io/github/MateusZanela/springstudy/SpringstudyApplication.java
@@ -2,17 +2,9 @@ package io.github.MateusZanela.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringstudyApplication {
-
-	@GetMapping("/hello-world")
-	public String helloWorld(){
-		return "Hello World. Spring boot application running";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringstudyApplication.class, args);
